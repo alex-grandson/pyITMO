a1e8887e91f6a58cb888a0159519a8a4ba3097bb
alex-grandson <285484@niuitmo.ru>
2021-08-25 11:43:42 +0300

    change func arg type

--- manager.py
+++ manager.py
@@ -1,8 +1,8 @@
 import telebot
 from telebot import types
+from typing import List
 
-
-def makeMarkupFromList(buttons: list):
+def makeMarkupFromList(buttons: List[str]):
     markup = types.ReplyKeyboardMarkup(row_width=1)
     for btn in buttons:
         markup.add(types.KeyboardButton(btn))
